1639b13a1cd2d0b5cf65cda43f892e0f4dfe8919
zhangir_ospan <ospan.zhangir2005@gmail.com>
2022-09-12 15:19:13 +0600

    footer finished

--- main.js
+++ main.js
@@ -29,3 +29,15 @@
     
   })();
   
+//   window.onscroll = function() {myFunction()};
+
+// let header = document.getElementById("myHeader");
+// let sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
