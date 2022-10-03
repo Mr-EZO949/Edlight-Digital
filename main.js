d2d76655144c7d7a2b47170a4caa5ada5bffc058
zhangir_ospan <ospan.zhangir2005@gmail.com>
2022-10-03 13:36:22 +0600

    explore buttons added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 (function () {
     const items = document.querySelectorAll(".timeline-section li");
   
@@ -29,6 +31,46 @@
     
   })();
   
+
+document.querySelectorAll('.accordion__button').forEach(button =>{
+  
+  button.addEventListener('click', () => {
+    console.log("fdsfds");
+    const accordionContent = button.parentElement.nextElementSibling;
+    button.classList.toggle("accordion__button--active");
+    const servicesContent = button.parentElement;
+    
+    if(button.classList.contains("accordion__button--active")){
+      accordionContent.style.maxHeight = accordionContent.scrollHeight +  "px";
+      servicesContent.classList.add("services-list-item--active");
+      servicesContent.classList.remove("services-list-item--closed");
+    }
+    else{
+      accordionContent.style.maxHeight = 0;
+      servicesContent.classList.remove("services-list-item--active");
+      servicesContent.classList.add("services-list-item--closed");
+    }
+  })
+});
+
+// var modal = document.querySelector(".modal");
+//     var trigger = document.querySelector(".trigger");
+//     var closeButton = document.querySelector(".close-button");
+
+//     function toggleModal() {
+//         modal.classList.toggle("show-modal");
+//     }
+
+//     function windowOnClick(event) {
+//         if (event.target === modal) {
+//             toggleModal();
+//         }
+//     }
+
+//     trigger.addEventListener("click", toggleModal);
+//     closeButton.addEventListener("click", toggleModal);
+//     window.addEventListener("click", windowOnClick);
+
 //   window.onscroll = function() {myFunction()};
 
 // let header = document.getElementById("myHeader");
